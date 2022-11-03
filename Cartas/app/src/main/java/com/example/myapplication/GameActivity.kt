00218622bf896d3f81d17647811cb5b7857e0d7e
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2022-11-03 23:39:46 +0100

    update

--- Cartas/app/src/main/java/com/example/myapplication/GameActivity.kt
+++ Cartas/app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
 import kotlin.math.roundToInt
 
 class GameActivity : AppCompatActivity(){
@@ -23,18 +25,45 @@ class GameActivity : AppCompatActivity(){
 
     private fun acciones() {
         var numRandom : Int
-        var carta : Drawable
         buttonUp.setOnClickListener(){
-            numRandom = ((Math.random() * 13)+1).roundToInt()
+            numRandom = ((Math.random() * 12)+1).roundToInt()
             Toast.makeText(applicationContext,"$numRandom",Toast.LENGTH_SHORT).show()
-            carta = "c"+numRandom
-            fondo.setBackgroundResource(R.drawable.c1)
+
+            when(numRandom){
+                1 -> fondo.setBackgroundResource(R.drawable.c1)
+                2 -> fondo.setBackgroundResource(R.drawable.c2)
+                3 -> fondo.setBackgroundResource(R.drawable.c3)
+                4 -> fondo.setBackgroundResource(R.drawable.c4)
+                5 -> fondo.setBackgroundResource(R.drawable.c5)
+                6 -> fondo.setBackgroundResource(R.drawable.c6)
+                7 -> fondo.setBackgroundResource(R.drawable.c7)
+                8 -> fondo.setBackgroundResource(R.drawable.c8)
+                9 -> fondo.setBackgroundResource(R.drawable.c9)
+                10 -> fondo.setBackgroundResource(R.drawable.c10)
+                11 -> fondo.setBackgroundResource(R.drawable.c11)
+                12 -> fondo.setBackgroundResource(R.drawable.c12)
+                13 -> fondo.setBackgroundResource(R.drawable.c13)
+            }
         }
         buttonDown.setOnClickListener(){
-            numRandom = ((Math.random() * 13)+1).roundToInt()
+            numRandom = ((Math.random() * 12)+1).roundToInt()
             Toast.makeText(applicationContext,"$numRandom",Toast.LENGTH_SHORT).show()
-            carta = R.drawable.$numRandom
-            fondo.bac(R.drawable.c1)
+
+            when(numRandom){
+                1 -> fondo.setBackgroundResource(R.drawable.c1)
+                2 -> fondo.setBackgroundResource(R.drawable.c2)
+                3 -> fondo.setBackgroundResource(R.drawable.c3)
+                4 -> fondo.setBackgroundResource(R.drawable.c4)
+                5 -> fondo.setBackgroundResource(R.drawable.c5)
+                6 -> fondo.setBackgroundResource(R.drawable.c6)
+                7 -> fondo.setBackgroundResource(R.drawable.c7)
+                8 -> fondo.setBackgroundResource(R.drawable.c8)
+                9 -> fondo.setBackgroundResource(R.drawable.c9)
+                10 -> fondo.setBackgroundResource(R.drawable.c10)
+                11 -> fondo.setBackgroundResource(R.drawable.c11)
+                12 -> fondo.setBackgroundResource(R.drawable.c12)
+                13 -> fondo.setBackgroundResource(R.drawable.c13)
+            }
         }
     }
 
