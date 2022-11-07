cd3315240a2787eea52fc1f08117e189f02d2b53
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2022-11-07 22:41:08 +0100

    update

--- Cartas/app/src/main/java/com/example/myapplication/GameActivity.kt
+++ Cartas/app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -1,24 +1,29 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.media.Image
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
 import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
 import java.io.File
 import kotlin.math.roundToInt
 
 class GameActivity : AppCompatActivity(){
     private lateinit var buttonUp : ImageButton
     private lateinit var buttonDown : ImageButton
-    private lateinit var fondo : LinearLayout
+    private lateinit var fondo : FrameLayout
+    private lateinit var textPuntos : TextView
     private var cartas : Array<Int> = arrayOf(R.drawable.cf, R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5, R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.c10, R.drawable.c11, R.drawable.c12, R.drawable.c13)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,20 +35,29 @@ class GameActivity : AppCompatActivity(){
     private fun acciones() {
         var numRandom : Int
         var numRandom2 : Int
-        numRandom = ((Math.random() * 12)+1).toInt()
+        var puntos : Int = 0
+        numRandom = (Math.random() * 13).toInt()
         fondo.setBackgroundResource(cartas[numRandom])
-        buttonUp.setOnClickListener(){
-            numRandom2 = ((Math.random() * 12) + 1).toInt()
 
+        if (buttonDown.isPressed){
+            buttonDown.setImageResource(R.drawable.down_press)
+        }
+
+        if (buttonUp.isHovered){
+            buttonUp.setImageResource(R.drawable.up_press)
+        }
+
+        buttonUp.setOnClickListener(){
+            numRandom2 = (Math.random() * 13).toInt()
+            fondo.setBackgroundResource(cartas[numRandom2])
             if (numRandom2 > numRandom){
                 println("MAYOR")
-                fondo.setBackgroundResource(cartas[numRandom2])
+                puntos++
+                textPuntos.setText("Puntos: $puntos")
             }else if (numRandom2 < numRandom){
                 println("MENOR")
-                fondo.setBackgroundResource(cartas[0])
-
-                val intent = Intent(this, MainActivity::class.java)
-
+                buttonUp.isEnabled = false
+                buttonDown.isEnabled = false
                 var notificacion =
                     Snackbar.make(buttonUp, "Juego terminado", Snackbar.LENGTH_INDEFINITE)
                     notificacion.setAction("¿Volver al inicio?") {
@@ -51,7 +65,29 @@ class GameActivity : AppCompatActivity(){
                         startActivity(intent)
                 }
                 notificacion.show()
-                startActivity(intent)
+            }
+
+            numRandom = numRandom2
+        }
+
+        buttonDown.setOnClickListener(){
+            numRandom2 = (Math.random() * 13).toInt()
+            fondo.setBackgroundResource(cartas[numRandom2])
+            if (numRandom2 < numRandom){
+                println("MENOR")
+                puntos++
+                textPuntos.setText("Puntos: $puntos")
+            }else if (numRandom2 > numRandom){
+                println("MAYOR")
+                buttonUp.isEnabled = false
+                buttonDown.isEnabled = false
+                var notificacion =
+                    Snackbar.make(buttonUp, "Juego terminado", Snackbar.LENGTH_INDEFINITE)
+                notificacion.setAction("¿Volver al inicio?") {
+                    var intent: Intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                notificacion.show()
             }
 
             numRandom = numRandom2
@@ -62,5 +98,6 @@ class GameActivity : AppCompatActivity(){
         buttonUp = this.findViewById(R.id.buttonUp)
         buttonDown = this.findViewById(R.id.buttonDown)
         fondo = this.findViewById(R.id.fondo)
+        textPuntos = this.findViewById(R.id.textPuntos)
     }
 }
